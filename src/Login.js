d25f2a5a724ad350af0bff4ce0ce9bc33d24a352
Amit Kumar <amitkumaroffic@gmail.com>
2021-11-08 11:47:50 +0530

    google login logout

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import {GoogleLogin,GoogleLogout } from "react-google-login";
+
+const Login =()=>{
+    const [showLoginButton,setShowLoginButton]=useState(true);
+    const [showLogoutButton,setShowLogoutButton]=useState(false);
+    
+    const onLoginSuccess=(res)=>{
+    console.log("Login Successfull",res.profileObj)
+   setShowLoginButton(false)
+   setShowLogoutButton(true)
+}
+
+   const onFailureSuccess=(res)=>{
+     console.log("Login Failed",res)
+   }
+
+   const onSignoutSuccess=()=>{
+       alert("Signed out Successfully")
+       setShowLoginButton(true)
+       setShowLogoutButton(false)
+       console.clear()
+   }
+
+   return(
+       <div>
+        {showLoginButton?
+        <GoogleLogin
+    clientId="623615460174-undrui8pi574v6qqtra0vo35vr58olqr.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={onLoginSuccess}
+    onFailure={onFailureSuccess}
+    cookiePolicy={'single_host_origin'}
+    />:null }
+
+     {showLogoutButton?
+        <GoogleLogout
+      clientId="623615460174-undrui8pi574v6qqtra0vo35vr58olqr.apps.googleusercontent.com"
+      buttonText="Logout"
+      onLogoutSuccess={onSignoutSuccess}
+    >
+    </GoogleLogout>:null}
+       </div>
+   )
+
+}
+
+export default Login;
